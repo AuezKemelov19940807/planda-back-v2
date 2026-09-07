86a3487cab807957ad4bf16d6a1645104a70170b
Auez <kemelovauez4@gmail.com>
2026-09-08 00:10:10 +0500

    update main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://business-digital-card-front.vercel.app',
+    ],
+    credentials: true,
+  });
+
+  const port = Number(process.env.PORT) || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(port, '0.0.0.0');
 }
 await bootstrap();
